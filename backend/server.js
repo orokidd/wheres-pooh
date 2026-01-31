5450191b8cb15a724ab31d72cae2780a544b79ae
orokidd <gregomily@gmail.com>
2026-01-31 16:48:55 +0800

    add to leaderboard function

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-/* global process */
 require('dotenv').config();
 const app = require('./src/app')
 
--- backend/src/controller/controller.js
+++ backend/src/controller/controller.js
@@ -1,3 +1,6 @@
+const prisma = require('../config/prisma')
+const API_KEY = process.env.API_KEY;
+
 const characters = {
   "waldo": { "xMin": 320, "xMax": 380, "yMin": 180, "yMax": 260 },
   "wizard": { "xMin": 500, "xMax": 560, "yMin": 300, "yMax": 370 }
@@ -5,6 +8,11 @@ const characters = {
 
 function checkUserInput(req, res) {
     const { x, y, character } = req.body;
+    const apiKey = req.headers.apiKey
+
+    if(apiKey !== API_KEY) {
+        return res.status(401).json({ success: false, message: "Unauthorized" })
+    }
 
     const target = characters[character]
     if (!target) return res.status(400).json({ success: false , message: "Invalid character."})
@@ -14,6 +22,29 @@ function checkUserInput(req, res) {
     res.json({ sucess: isFound })
 }
 
+async function addToLeaderboard(req, res) {
+    const { username, time } = req.body;
+    const apiKey = req.headers.apiKey
+
+    if(apiKey !== API_KEY) {
+        return res.status(401).json({ success: false, message: "Unauthorized" })
+    }
+
+    if (!username || !time) {
+        return res.status(400).json({ success: false, message: "Invalid username or time" })
+    }
+
+    await prisma.leaderboard.create({
+        data: {
+            username,
+            time
+        }
+    })
+
+    return res.status(200).json({ success: true, message: "Add data success" })
+}
+
 module.exports = {
-    checkUserInput
+    checkUserInput,
+    addToLeaderboard
 }
