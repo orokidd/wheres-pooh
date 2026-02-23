59b02faec1c79e76933699cfd85468558e656208
orokidd <gregomily@gmail.com>
2026-02-24 03:21:15 +0800

    character selection state

--- backend/src/controller/controller.js
+++ backend/src/controller/controller.js
@@ -1,50 +1,52 @@
-const prisma = require('../config/prisma')
+const prisma = require("../config/prisma");
 const API_KEY = process.env.API_KEY;
 
 const characters = {
-  "waldo": { "xMin": 320, "xMax": 380, "yMin": 180, "yMax": 260 },
-  "wizard": { "xMin": 500, "xMax": 560, "yMin": 300, "yMax": 370 }
-}
+	Aladdin: { xMin: 320, xMax: 380, yMin: 180, yMax: 260 },
+	Merlin: { xMin: 500, xMax: 560, yMin: 300, yMax: 370 },
+	Rapunzel: { xMin: 320, xMax: 380, yMin: 180, yMax: 260 },
+	Pooh: { xMin: 500, xMax: 560, yMin: 300, yMax: 370 },
+};
 
 function checkUserInput(req, res) {
-    const { x, y, character } = req.body;
-    const apiKey = req.headers.apiKey
+	const { x, y, character } = req.body;
+	const apiKey = req.headers.apiKey;
 
-    if(apiKey !== API_KEY) {
-        return res.status(401).json({ success: false, message: "Unauthorized" })
-    }
+	if (apiKey !== API_KEY) {
+		return res.status(401).json({ success: false, message: "Unauthorized" });
+	}
 
-    const target = characters[character]
-    if (!target) return res.status(400).json({ success: false , message: "Invalid character."})
-    
-    const isFound = x >= target.xMin && x <= target.xMax && y >= target.yMin && y <= target.yMax;
+	const target = characters[character];
+	if (!target) return res.status(400).json({ success: false, message: "Invalid character." });
 
-    res.json({ sucess: isFound })
+	const isFound = x >= target.xMin && x <= target.xMax && y >= target.yMin && y <= target.yMax;
+
+	res.json({ sucess: isFound, character });
 }
 
 async function addToLeaderboard(req, res) {
-    const { username, time } = req.body;
-    const apiKey = req.headers.apiKey
+	const { username, time } = req.body;
+	const apiKey = req.headers.apiKey;
 
-    if(apiKey !== API_KEY) {
-        return res.status(401).json({ success: false, message: "Unauthorized" })
-    }
+	if (apiKey !== API_KEY) {
+		return res.status(401).json({ success: false, message: "Unauthorized" });
+	}
 
-    if (!username || !time) {
-        return res.status(400).json({ success: false, message: "Invalid username or time" })
-    }
+	if (!username || !time) {
+		return res.status(400).json({ success: false, message: "Invalid username or time" });
+	}
 
-    await prisma.leaderboard.create({
-        data: {
-            username,
-            time
-        }
-    })
+	await prisma.leaderboard.create({
+		data: {
+			username,
+			time,
+		},
+	});
 
-    return res.status(200).json({ success: true, message: "Add data success" })
+	return res.status(200).json({ success: true, message: "Add data success" });
 }
 
 module.exports = {
-    checkUserInput,
-    addToLeaderboard
-}
+	checkUserInput,
+	addToLeaderboard,
+};
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,75 +1,92 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import siteLogo from "./assets/site-icon.jpeg";
-import puzzleImage from "./assets/main-img.jpg"
+import puzzleImage from "./assets/main-img.jpg";
+
+type Status = {
+	name: string;
+	found: boolean;
+};
+
+type Seleciton = {
+	x: number;
+	y: number;
+	character: string;
+};
+
+const characterNames: string[] = ["Aladdin", "Merlin", "Rapunzel", "Pooh"];
 
 function App() {
-	type Status = {
-		name: string;
-		found: boolean
-	}
-
-	type Seleciton = {
-		x: number;
-		y: number;
-		character: string;
-	}
-
 	const [welcomePage, setWelcomePage] = useState<boolean>(true);
-	const [characterSelectionCard, setCharacterSelectionCard] = useState<boolean>(false)
+	const [characterSelectionCard, setCharacterSelectionCard] = useState<boolean>(false);
+
 	const [gameStatus, setGameStatus] = useState<Status[]>([
 		{
 			name: "Aladdin",
-			found: false
+			found: false,
 		},
 		{
 			name: "Merlin",
-			found: false
+			found: false,
 		},
 		{
 			name: "Rapunzel",
-			found: false
+			found: false,
 		},
 		{
 			name: "Pooh",
-			found: false
+			found: false,
 		},
-	])
+	]);
 
 	const [currentSelection, setCurrentSelection] = useState<Seleciton>({
-		x : 0,
-		y : 0,
-		character : ""
-	})
+		x: 0,
+		y: 0,
+		character: "",
+	});
 
-	async function handleImageClick(e: React.MouseEvent<HTMLImageElement>) {
-		const imageInfo = e.currentTarget.getBoundingClientRect()
-		const imageLeftGap = imageInfo.left
-		const imageTopGap = imageInfo.top
-		const currentMouseCoordinatesX = e.clientX 
-		const currentMouseCoordinatesY = e.clientY
+	function handleImageClick(e: React.MouseEvent<HTMLImageElement>) {
+		const imageInfo = e.currentTarget.getBoundingClientRect();
+		const imageLeftGap = imageInfo.left;
+		const imageTopGap = imageInfo.top;
+		const currentMouseCoordinatesX = e.clientX;
+		const currentMouseCoordinatesY = e.clientY;
 
-		const x = currentMouseCoordinatesX - imageLeftGap
-		const y = currentMouseCoordinatesY - imageTopGap
+		const x = currentMouseCoordinatesX - imageLeftGap;
+		const y = currentMouseCoordinatesY - imageTopGap;
 
 		setCurrentSelection((prev) => {
-			return {...prev, x, y}
-		})
+			return { ...prev, x, y };
+		});
 
-		setCharacterSelectionCard(true)
-		// const result:boolean = await checkCharacter({
-		// 	x,
-		// 	y,
-		// 	character
-		// })
+		setCharacterSelectionCard(true);
+	}
+
+	async function handleSelectionClick(e: React.MouseEvent) {
+		const selectedCharacter = e.currentTarget.id;
+
+		setCurrentSelection((prev) => {
+			return { ...prev, character: selectedCharacter };
+		});
+
+		const updatedSelection = {
+			...currentSelection,
+			character: selectedCharacter,
+		};
+
+		setCurrentSelection(updatedSelection);
+
+		console.log(updatedSelection);
+
+		//   const result: boolean = await checkCharacter(updatedSelection);
 	}
 
 	if (welcomePage === true)
 		return (
-			<div>
+			<div className="welcome">
 				<p>Where's Pooh</p>
-				<button onClick={() => { setWelcomePage(false); }}>
-					Change
+				<button onClick={() => { setWelcomePage(false) }}>
+					Start Game
 				</button>
 			</div>
 		);
@@ -88,23 +105,31 @@ function App() {
 				</div>
 			</header>
 
-			<section>
+			<section className="game-instruction">
 				<h1>Where's Pooh?</h1>
 				<p>In order to finish the game, you have to find Winnie the Pooh and all his friends.</p>
 			</section>
 
 			<main>
 				<div className="image-container">
-					<img src={puzzleImage} alt="Puzzle Image" onClick={handleImageClick}/>
+					<img src={puzzleImage} alt="Puzzle Image" onClick={handleImageClick} />
 				</div>
 			</main>
 
-			<div className="character-seleciton">
-				<button value="Aladdin">Aladdin</button>
-				<button value="Merlin">Merlin</button>
-				<button value="Rapunzel">Rapunzel</button>
-				<button value="Pooh">Pooh</button>
-			</div>
+			{characterSelectionCard ? (
+				<div className="character-seleciton">
+					<div className="close-btn">
+						<button onClick={() => setCharacterSelectionCard(false)}>X</button>
+					</div>
+					<div className="options">
+						{characterNames.map((char, index) => (
+							<button value={char} id={char} onClick={handleSelectionClick} key={index}>
+								{char}
+							</button>
+						))}
+					</div>
+				</div>
+			) : null}
 		</>
 	);
 }
